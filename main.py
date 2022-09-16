28e934aab397f9374c1f50a3bdf7dae4126cc251
MrQuackDuck <mrquackduckk@gmail.com>
2022-09-16 23:54:16 +0300

    Now methods can accept negative key

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 class Encryptor:
     def __init__(self, Index, AllSymbols):
         self.Index = int(Index)
-        self.AllSymbols = AllSymbols
+        self.AllowedSymbols = AllSymbols
 
     def EncryptWithCaesarCipher(self, message):
         resultString = ""
-        for char in message:
-            indexOfCurrentChar = self.AllSymbols.index(char)
+        allowedSymbolsLength = len(self.AllowedSymbols)
 
-            if (indexOfCurrentChar + self.Index) >= len(self.AllSymbols):
-                rest = (indexOfCurrentChar - self.Index) - len(self.AllSymbols)
-                resultString = resultString + self.AllSymbols[rest]
+        for char in message:
+            indexOfCurrentChar = self.AllowedSymbols.index(char)
+
+            if (indexOfCurrentChar + self.Index) >= allowedSymbolsLength:
+                rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
+                resultString += self.AllowedSymbols[rest]
+            elif indexOfCurrentChar + self.Index <= 0:
+                rest = allowedSymbolsLength + (indexOfCurrentChar + self.Index)
+                resultString += self.AllowedSymbols[rest]
             else:
-                resultString += self.AllSymbols[indexOfCurrentChar + self.Index]
+                resultString += self.AllowedSymbols[indexOfCurrentChar + self.Index]
         return  resultString
     def DecryptWithCaesarCipher(self, message):
         resultString = ""
+        allowedSymbolsLength = len(self.AllowedSymbols)
         for char in message:
-            indexOfCurrentChar = self.AllSymbols.index(char)
+            indexOfCurrentChar = self.AllowedSymbols.index(char)
 
             if (indexOfCurrentChar - self.Index) < 0:
-                rest = (indexOfCurrentChar - self.Index) - len(self.AllSymbols)
-                resultString += self.AllSymbols[rest]
+                rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
+                resultString += self.AllowedSymbols[rest]
+            elif (indexOfCurrentChar - self.Index) > allowedSymbolsLength:
+                rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
+                resultString += self.AllowedSymbols[rest]
             else:
-                resultString = resultString + self.AllSymbols[indexOfCurrentChar - self.Index]
+                resultString = resultString + self.AllowedSymbols[indexOfCurrentChar - self.Index]
         return resultString
 
 
+index = -3
 
-caesarEncryptor = Encryptor(3, " abcdefghijklmnopqrstuvwxyzабвгґдеєжзиіїйклмнопрстуфхцчшщьюя123456789!&?_\/")
+caesarEncryptor = Encryptor(index, " abcdefghijklmnopqrstuvwxyzабвгґдеєжзиіїйклмнопрстуфхцчшщьюя123456789!&?_\/';@$%^~`")
 message = input("Write your message: ")
 
 encryptedMessage = caesarEncryptor.EncryptWithCaesarCipher(message)
