479b2c77cbbb3dc071a2f05ef11ee22c1b33f08e
MrQuackDuck <mrquackduckk@gmail.com>
2022-09-17 19:51:48 +0300

    Added comments and made some fixes

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ class Encryptor:
         allowedSymbolsLength = len(self.AllowedSymbols)
 
         for char in message:
+            # Index of current char in allowed symbols
             indexOfCurrentChar = self.AllowedSymbols.index(char)
 
+            # Checks if current char index is not out of bounds of allowed symbols list
             if (indexOfCurrentChar + self.Index) >= allowedSymbolsLength:
                 rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
                 resultString += self.AllowedSymbols[rest]
-            elif indexOfCurrentChar + self.Index <= 0:
+            elif indexOfCurrentChar + self.Index < 0:
                 rest = allowedSymbolsLength + (indexOfCurrentChar + self.Index)
                 resultString += self.AllowedSymbols[rest]
             else:
                 resultString += self.AllowedSymbols[indexOfCurrentChar + self.Index]
         return  resultString
+
     def DecryptWithCaesarCipher(self, message):
         resultString = ""
         allowedSymbolsLength = len(self.AllowedSymbols)
         for char in message:
             indexOfCurrentChar = self.AllowedSymbols.index(char)
 
+            # Checks if current char index is not out of bounds of allowed symbols list
             if (indexOfCurrentChar - self.Index) < 0:
                 rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
                 resultString += self.AllowedSymbols[rest]
-            elif (indexOfCurrentChar - self.Index) > allowedSymbolsLength:
+            elif (indexOfCurrentChar - self.Index) >= allowedSymbolsLength:
                 rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
                 resultString += self.AllowedSymbols[rest]
             else:
@@ -38,7 +42,7 @@ class Encryptor:
 
 index = -3
 
-caesarEncryptor = Encryptor(index, " abcdefghijklmnopqrstuvwxyzабвгґдеєжзиіїйклмнопрстуфхцчшщьюя123456789!&?_\/';@$%^~`")
+caesarEncryptor = Encryptor(index, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгґдеєжзиіїйклмнопрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ123456789!&?_\/';@$%^~`,. ")
 message = input("Write your message: ")
 
 encryptedMessage = caesarEncryptor.EncryptWithCaesarCipher(message)
