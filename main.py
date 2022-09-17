6c1b02fde5a1f97ef0f607722a5d4c91ac4b552f
MrQuackDuck <mrquackduckk@gmail.com>
2022-09-17 20:56:13 +0300

    Bugfix

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Encryptor:
 
             # Checks if current char index is not out of bounds of allowed symbols list
             if (indexOfCurrentChar + self.Index) >= allowedSymbolsLength:
-                rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
+                rest = (indexOfCurrentChar + self.Index) - allowedSymbolsLength
                 resultString += self.AllowedSymbols[rest]
             elif indexOfCurrentChar + self.Index < 0:
                 rest = allowedSymbolsLength + (indexOfCurrentChar + self.Index)
@@ -30,7 +30,7 @@ class Encryptor:
 
             # Checks if current char index is not out of bounds of allowed symbols list
             if (indexOfCurrentChar - self.Index) < 0:
-                rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
+                rest = allowedSymbolsLength + (indexOfCurrentChar - self.Index)
                 resultString += self.AllowedSymbols[rest]
             elif (indexOfCurrentChar - self.Index) >= allowedSymbolsLength:
                 rest = (indexOfCurrentChar - self.Index) - allowedSymbolsLength
@@ -40,9 +40,9 @@ class Encryptor:
         return resultString
 
 
-index = -3
+index = 2
 
-caesarEncryptor = Encryptor(index, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгґдеєжзиіїйклмнопрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ123456789!&?_\/';@$%^~`,. ")
+caesarEncryptor = Encryptor(index, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгґдеєжзиіїйклмнопрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ123456789!&?_\/';@$%^~`,.{}[]` ")
 message = input("Write your message: ")
 
 encryptedMessage = caesarEncryptor.EncryptWithCaesarCipher(message)
